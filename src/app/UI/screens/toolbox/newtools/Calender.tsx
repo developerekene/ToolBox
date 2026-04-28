72aafe8fa17f37f6367c064f665b98cced18df5a
developerekene <developerekene@gmail.com>
2026-04-28 21:22:59 +0100

    reverting form pull

--- src/app/UI/screens/toolbox/newtools/Calender.tsx
+++ src/app/UI/screens/toolbox/newtools/Calender.tsx
@@ -1,10 +1,10 @@
 import { View, Text } from "react-native";
 import React from "react";
 
-const Calender = () => {
+const Calender: React.FunctionComponent = () => {
   return (
     <View>
-      <Text>Calender</Text>
+      <Text>This is the callender app</Text>
     </View>
   );
 };
