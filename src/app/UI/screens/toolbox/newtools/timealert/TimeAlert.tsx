53260815816dbf362ff3f57b09208ae0c52c01cd
stellanechi <stellanechi65@gmail.com>
2026-05-12 12:35:49 +0100

    updated

--- src/app/UI/screens/toolbox/newtools/timealert/TimeAlert.tsx
+++ src/app/UI/screens/toolbox/newtools/timealert/TimeAlert.tsx
@@ -1,41 +1,27 @@
-// import React, { useState } from "react";
-// import { View, StyleSheet, SafeAreaView, StatusBar } from "react-native";
-// import { T, Mode } from "../../../../../utils/constant/timealert/theme";
-// import { TabBar } from "../../newtools/timealert/TimerComponent";
-// import AlarmScreen from "../../newtools/timealert/AlarmScreen";
-// import StopwatchScreen from "../../newtools/timealert/StopWatchScreen";
-// import TimerScreen from "../../newtools/timealert/TimerScreen";
+import React, { useState } from "react";
+import { View, StyleSheet, SafeAreaView, StatusBar } from "react-native";
+import { T, Mode } from "../../../../../utils/constant/timealert/theme";
+import { TabBar } from "../../newtools/timealert/TimerComponent";
+import AlarmScreen from "../../newtools/timealert/AlarmScreen";
+import StopwatchScreen from "../../newtools/timealert/StopWatchScreen";
+import TimerScreen from "../../newtools/timealert/TimerScreen";
 
-// export default function TimeAlert() {
-//   const [mode, setMode] = useState<Mode>("timer");
+export default function TimeAlert() {
+  const [mode, setMode] = useState<Mode>("timer");
 
-//   return (
-//     <View style={styles.root}>
-//       <TabBar mode={mode} onChange={setMode} />
-//       <View style={styles.content}>
-//         {mode === "alarm" && <AlarmScreen />}
-//         {mode === "stopwatch" && <StopwatchScreen />}
-//         {mode === "timer" && <TimerScreen />}
-//       </View>
-//     </View>
-//   );
-// }
-
-// const styles = StyleSheet.create({
-//   root: { flex: 1, backgroundColor: T.bg },
-//   content: { flex: 1 },
-// });
-
-import { View, Text } from "react-native";
-import React from "react";
-import ComingSoon from "../../../../component/CoomingSoon";
-
-const TimeAlert = () => {
   return (
-    <View>
-      <ComingSoon />
+    <View style={styles.root}>
+      <TabBar mode={mode} onChange={setMode} />
+      <View style={styles.content}>
+        {mode === "alarm" && <AlarmScreen />}
+        {mode === "stopwatch" && <StopwatchScreen />}
+        {mode === "timer" && <TimerScreen />}
+      </View>
     </View>
   );
-};
+}
 
-export default TimeAlert;
+const styles = StyleSheet.create({
+  root: { flex: 1, backgroundColor: T.bg },
+  content: { flex: 1 },
+});
